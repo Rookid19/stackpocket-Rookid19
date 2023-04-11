5ddf13d8209b714373dd4a371f8b1951fa7b2360
Randy Odoom <63527175+Rookid19@users.noreply.github.com>
2023-04-11 14:57:26 +0000

    registration form

---
+++ registration.ts
@@ -0,0 +1,18 @@
+ /**
+ *
+ *
+ * @param {string} email
+ * @param {string} password
+ * @param {string} userName
+ * @param {string} fullName
+ * @param {string} confirmPassword
+ */
+ export const Register = async (
+  email: string,
+  password: string,
+  userName: string,
+  fullName: string,
+  confirmPassword: string
+) => {
+console.log(email, password, userName, fullName, confirmPassword)
+};
